af77e8caa73c39f3bab0349b8372c821aa440cba
Brianojunga <brianojunga3@gmail.com>
2025-05-14 22:59:01 +0300

    Add function components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import Header from "./Header";
-import AppContextProvider from "./Context/context";
+import Header from "./main/Header";
+import AppContextProvider from "./Context/AppContext";
+import Todos from "./main/todos";
+import MainContextProvider from "./Context/mainContext";
+import Footer from "./main/Footer";
 
 function App() {
   return (
-    <AppContextProvider>
-      <Header />
-    </AppContextProvider>
+    <>
+      <AppContextProvider>
+        <Header />
+        <MainContextProvider>
+          <Todos />
+        </MainContextProvider>
+      </AppContextProvider>
+      <Footer />
+    </>
   );
 }
 
