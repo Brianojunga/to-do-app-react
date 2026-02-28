59c799d2d7561d1632efb33d4e9a9e3682e324a4
Brianojunga <brianojunga3@gmail.com>
2026-02-28 20:02:56 +0300

    upate the outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,3 +84,7 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
 output "cloudfront_domain_name" {
     value = aws_cloudfront_distribution.frontend_distribution.domain_name
 }
+
+output "s3_bucket_name" {
+    value = aws_s3_bucket.frontend_bucket.bucket
+}
